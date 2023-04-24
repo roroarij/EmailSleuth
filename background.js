a50f6327ab53003482526e42594ab836cc48c93f
roroarij <rosenarij@gmail.com>
2023-04-23 21:01:44 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // background.js
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({todoList: []}, function() {
-        console.log('Todo list initialized.');
+        console.log('Todo list is initialized.');
     });
 });
 
